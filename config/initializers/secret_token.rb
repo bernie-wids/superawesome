2e4d3a713baaed38450e3f217f54e526c42783b2
Bernard Agustino Widjanarko <bernard.widjanarko@gmail.com>
2011-05-27 08:59:21 +0700

    initiate rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Superawesome::Application.config.secret_token = '6cfa1fa626a0d0bb34496ca64f6e489cfa9ac5eb2d967afb6e00962833cdfb2f03fbe8f464ff7b2c8707003e4819e5c5d555fdbbb142b353e5196294424b7eeb'
