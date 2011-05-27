e54abd8c8b824177fa7ac3367af9f86fb675d197
Bernard Agustino Widjanarko <bernard.widjanarko@gmail.com>
2011-05-27 12:55:01 +0700

    fix if param :locale is nil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
   
   def set_locale
-    I18n.locale = params[:locale]
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
